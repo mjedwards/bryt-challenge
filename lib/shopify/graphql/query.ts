9275debaf37fb62610e16a0df158e24d85be6e37
mjedwards <jairedwards20@gmail.com>
2026-02-23 14:31:28 -0500

    setup the general file layout and added product fetching using pagination

--- lib/shopify/graphql/query.ts
+++ lib/shopify/graphql/query.ts
@@ -1,8 +1,42 @@
+// get general shop information
 export const getShop = `#graphql
   query getShop {
     shop {
       name
       description
+
     }
   }
+` as const;
+
+// get product information for shop
+export const getProducts = `#graphql
+  query getProducts($first: Int!, $after: String) {
+    products(first: $first, after: $after) {
+      edges {
+      cursor
+      node {
+        id
+        title
+        handle
+        availableForSale
+        createdAt
+        priceRange {
+          minVariantPrice {
+            amount
+            currencyCode
+          }
+          maxVariantPrice {
+            amount
+            currencyCode
+          }
+        }
+      }
+    }
+    pageInfo {
+      hasNextPage
+      endCursor
+    }
+    }
+}
 ` as const;
